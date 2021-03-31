60d6b5ab34daa4ed0cf68e5b45778436fd56c097
Dominique Eberhard <de97fb@yahoo.com>
2021-03-31 03:53:11 +0200

    Initial Commit

---
+++ SwiftUIAnimationPractice/SwiftUIAnimationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationPracticeApp.swift
+//  SwiftUIAnimationPractice
+//
+//  Created by Dominique Eberhard on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
